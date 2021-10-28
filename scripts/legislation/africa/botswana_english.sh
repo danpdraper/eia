ad842c98ad14ffafc937014316b3c7e3d9d0f3c0
Skyrolon <skyrolon@users.noreply.github.com>
2021-10-28 16:59:22 -0700

    Revision #1

--- scripts/legislation/africa/botswana_english.sh
+++ scripts/legislation/africa/botswana_english.sh
@@ -17,17 +17,13 @@ function replace_parentheses_around_article_delimiters_with_square_brackets {
   sed -E 's/^\(([A-Za-z0-9]+)\)/[\1]/' 
 }
 
-function move_article_titles_above_article_bodies {
-  sed -E 's/^(\([0-9]+\)) ([^|]+)\|/\2\n\1/' 
-}
-
 function remove_all_text_after_last_article {
   sed -E '/^\(32\) /,${/^\(32\)/!d}' | \
-  sed -E 's/\(32\) Articles deemed.*//'
+    sed -E '/^\(32\) Articles deemed.*/d'
 }
 
 function amend_errors_in_headers {
-    sed -E 's/PART I Preliminary /PART I - Preliminary /' | \
+  sed -E 's/PART I Preliminary /PART I - Preliminary /' | \
     sed -E 's/PART II Preparation/\n\nPART II - Preparation/' | \
     sed -E 's/PART III Review/\n\nPART III - Review/' | \
     sed -E 's/PART IV Authorisation/\n\nPART IV - Authorisation/' | \
@@ -43,14 +39,17 @@ function amend_errors_in_articles {
     sed -E 's/,000/000/g' | \
     sed -E "s/^(\([0-9]+\).*)- /\1 /" | \
     sed -E 's/\[•\] //g' | \
+    #Article 2
+    amend_error_in_article 2 'requires "' 'requires: "' | \
     #Article 3
     amend_error_in_article 3 '\(a\)' '[a]' | \
     amend_error_in_article 3 "screening''" '"screening"' | \
     #Article 4
-    amend_error_in_article 4 '14\]' '14].' | \
+    amend_error_in_article 4 '\[14\]' '14.' | \
     amend_error_in_article 4 '\(5\)' '\[5\]' | \
     #Article 5
     amend_error_in_article 5 '3\]' '3].' | \
+    amend_error_in_article 5 '\[3\].' '3.' | \
     #Article 6
     amend_error_in_article 6 '\[1\] \(a\)' '[1][a]' | \
     #Article 12
@@ -63,8 +62,8 @@ function amend_errors_in_articles {
     amend_error_in_article 14 '\(a\)' '[a]' | \
     amend_error_in_article 14 '\(b\)' '[b]' | \
     amend_error_in_article 14 '\[1\] \[a\]' '[1][a]' | \
-    sed -E ':start;s/^(\(14\).*)\(ii\)/\1[ii] /;t start' | \
-    sed -E ':start;s/^(\(14\).*)\(ii\)/\1[ii] /;t start' | \
+    sed -E ':start;s/^(\(14\).*)\(i\)/\1[i]/;t start' | \
+    sed -E ':start;s/^(\(14\).*)\(ii\)/\1[ii]/;t start' | \
     sed -E ':start;s/^(\(14\).*)-/\1/;t start' | \
     #Article 16
     amend_error_in_article 16 ' \[1\] \(a\)' '[1][a]' | \
@@ -76,6 +75,10 @@ function amend_errors_in_articles {
     #Article 26
     amend_error_in_article 26 'subsection\(1\)' 'subsection [1]' | \
     amend_error_in_article 26 '\(b\)' '[b]' 
+}
+
+function move_article_titles_above_article_bodies {
+  sed -E 's/^(\([0-9]+\)) ([^|]+)\|/\2\n\1/' 
 }
 
 function preprocess_state_and_language_input_file {
