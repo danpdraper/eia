cc9a5cda28867b7e69d5c42e9f9df9255ce6157b
Daniel Draper <danpdraper@gmail.com>
2022-02-03 09:13:21 -0800

    Removed footers from preprocessed French version of legislation from Central African Republic

--- scripts/legislation/africa/central_african_republic_french.sh
+++ scripts/legislation/africa/central_african_republic_french.sh
@@ -9,13 +9,13 @@ function remove_all_text_before_first_title_header {
 }
 
 function amend_errors_in_headers {
-  sed -E 's/^(Section [0-9]+ - [A-Za-z,’ ]+)[^a-z]+Art\. ([0-9]+):/\1\n\n(\2)/' | \
+  sed -E "s/^(Section [0-9]+ - [A-Za-z,' ]+)[^a-z]+Art\. ([0-9]+):/\1\n\n(\2)/" | \
     sed -E 's/^(Section) I(.*) Art\. 103:/\1 1\2\n\n(103)/' | \
     sed -E 's/^(TITRE I .*)GENERALES/\1GÉNÉRALES/' | \
     sed -E 's/^(CHAPITRE I - DES )DEFINITIONS/\1DÉFINITIONS/' | \
     sed -E 's/^(Section 2 - Des substances et )Produits/\1produits/' | \
     sed -E 's/^(Section 7 - Des )Etudes/\1Études/' | \
-    sed -E 's/^(Section 9 - De l’)Evaluation/Évaluation/' | \
+    sed -E "s/^(Section 9 - De l')Evaluation/\1Évaluation/" | \
     sed -E 's/^(TITRE IV .*)REPRESSION/\1RÉPRESSION/'
 }
 
@@ -23,16 +23,17 @@ function amend_errors_in_articles {
   local stdin="$(</dev/stdin)"
 
   local footer_regular_expression='J\.O\.R\.C\.A\. \/ MARS 2008 EDITION '
-  footer_regular_expression+='SPECIALE LOI PORTANT CODE DE L’ENVIRONNEMENT DE '
+  footer_regular_expression+="SPECIALE LOI PORTANT CODE DE L'ENVIRONNEMENT DE "
   footer_regular_expression+='LA REPUBLIQUE CENTRAFRICAINE [0-9]+ '
 
   echo "$stdin" | \
     sed -E "s/${footer_regular_expression}//g" | \
     # Article 3
     amend_error_in_article 3 'AIRE PROTEGEE' 'AIRE PROTÉGÉE' | \
+    amend_error_in_article 3 'micros organismes' microorganismes | \
     amend_error_in_article 3 'DECHET:' 'DÉCHET:' | \
-    amend_error_in_article 3 'DEGRADATION DE L’ENVIRONNEMENT' 'DÉGRADATION DE L’ENVIRONNEMENT' | \
-    amend_error_in_article 3 'DEGRADATION DE L’ESTHETIQUE' "DÉGRADATION DE L’ESTHÉTIQUE" | \
+    amend_error_in_article 3 "DEGRADATION DE L'ENVIRONNEMENT" "DÉGRADATION DE L'ENVIRONNEMENT" | \
+    amend_error_in_article 3 "DEGRADATION DE L'ESTHETIQUE" "DÉGRADATION DE L'ESTHÉTIQUE" | \
     amend_error_in_article 3 'DEVELOPPEMENT DURABLE' 'DÉVELOPPEMENT DURABLE' | \
     amend_error_in_article 3 DIVERSITE DIVERSITÉ | \
     amend_error_in_article 3 'EDUCATION ENVIRONNEMENTALE' 'ÉDUCATION ENVIRONNEMENTALE' | \
@@ -42,7 +43,7 @@ function amend_errors_in_articles {
     amend_error_in_article 3 'ELIMINATION DES DECHETS' 'ÉLIMINATION DES DÉCHETS' | \
     amend_error_in_article 3 'EQUILIBRE ECOLOGIQUE' 'ÉQUILIBRE ÉCOLOGIQUE' | \
     amend_error_in_article 3 ETABLISSEMENTS ÉTABLISSEMENTS | \
-    amend_error_in_article 3 'ETUDES D’IMPACT' 'ÉTUDES D’IMPACT' | \
+    amend_error_in_article 3 "ETUDES D'IMPACT" "ÉTUDES D'IMPACT" | \
     amend_error_in_article 3 'ECOLOGIQUEMENT RATIONNELLE DES DECHETS' 'ÉCOLOGIQUEMENT RATIONNELLE DES DÉCHETS' | \
     amend_error_in_article 3 'INSTALLATIONS CLASSEES' 'INSTALLATIONS CLASSÉES' | \
     amend_error_in_article 3 'VIVANTS MODIFIES' 'VIVANTS MODIFIÉS' | \
@@ -55,6 +56,7 @@ function amend_errors_in_articles {
     amend_error_in_article 16 ', dressent' ', dresse' | \
     # Article 28
     amend_error_in_article 28 'zones de protections' 'zones de protection' | \
+    amend_error_in_article 28 'au dessus' 'au-dessus' | \
     # Article 30
     amend_error_in_article 30 'mobile, \[•\]' 'mobile; [•]' | \
     # Article 35
@@ -72,7 +74,7 @@ function amend_errors_in_articles {
     # Article 75
     amend_error_in_article 75 '\[77\]' '77.' | \
     # Article 88
-    amend_error_in_article 88 'L’Etude' 'L’Étude' | \
+    amend_error_in_article 88 "L'Etude" "L'Étude" | \
     # Article 89
     amend_error_in_article 89 Etudes Études | \
     # Article 90
@@ -86,10 +88,15 @@ function amend_errors_in_articles {
     # Article 102
     amend_error_in_article 102 standard standards | \
     # Article 106
-    amend_error_in_article 106 'procès \[•\] verbaux' 'procès - verbaux' | \
-    amend_error_in_article 106 'procès \[•\] verbaux' 'procès - verbaux' | \
+    amend_error_in_article 106 'procès \[•\] verbaux' 'procès-verbaux' | \
+    amend_error_in_article 106 'procès \[•\] verbaux' 'procès-verbaux' | \
+    # Article 107
+    amend_error_in_article 107 'procès – verbaux' 'procès-verbaux' | \
     # Article 108
+    amend_error_in_article 108 'procès – verbaux' 'procès-verbaux' | \
     amend_error_in_article 108 cidessus 'ci-dessus' | \
+    # Article 109
+    amend_error_in_article 109 'procès – verbal' 'procès-verbal' | \
     # Article 116
     amend_error_in_article 116 cidessus 'ci-dessus' | \
     # Article 117
