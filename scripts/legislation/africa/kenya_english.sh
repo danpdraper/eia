706b862fc8002753af4a1ab4e2f39fdd3d96ed1b
Skyrolon <skyrolon@users.noreply.github.com>
2022-03-15 20:43:57 -0700

    Revised as per older legislation

--- scripts/legislation/africa/kenya_english.sh
+++ scripts/legislation/africa/kenya_english.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
 function remove_all_text_before_first_header {
-  sed -n '/^economic, social, cultural and spiritual advancement;/,$p' | \
-    sed -n '/^PART I – PRELIMINARY/,$p'
+  sed -n '/ NOW THEREFORE/,$p' | \
+    sed -n '/^PART I - PRELIMINARY/,$p'
 }
 
 function prefix_article_numbers_with_article_literal {
   sed -E 's/^([0-9]+)/Article \1/' 
 }
 
-function append_pipe_to_article_title {
-  sed -E 's/^(Article .*)$/\1|/' 
-}
-
 function replace_parentheses_around_article_delimiters_with_square_brackets {
   sed -E 's/\(([A-Za-z0-9]+)\)/[\1]/g' 
-}
-
-function move_article_titles_above_article_bodies {
-  sed -E 's/^(\([0-9]+\)) ([^|]+)\|/\2\n\1/'
 }
 
 function remove_all_text_after_last_article {
@@ -26,92 +18,114 @@ function remove_all_text_after_last_article {
 }
 
 function amend_errors_in_articles {
-  sed -E 's/— /: /g' | \
-    sed -E 's/\[Issue 1\] //g' | \
-    sed -E 's/ E12 \[•\] [0-9]+//g' | \
-    sed -E 's/\[Issue//g' | \
-    sed -E 's/CAP\. 387 Environmental Management and Co-ordination \[Rev. 2012\] //g' | \
-    sed -E 's/\[Rev. 2012\] Environmental Management and Co-ordination CAP\. 387 //g' | \
+  sed -E 's/ –/:/g' | \
+    sed -E 's/ 1\]/ [1]/g' | \
+    sed -E 's/Article [0-9]+ //g' | \
+    sed -E 's/:-/:/g' | \
+    sed -E 's/ section \[([0-9]+)\]/ section \1/g' | \
+    #Article 1
+    amend_error_in_article 1 'Article' '1999.' | \
     #Article 2
-    amend_error_in_article 2 '\[•\] 12 ' '' | \
-    amend_error_in_article 2 'microrganism' 'microorganism' | \
-    amend_error_in_article 2 '\[171]' '171)' | \
-    amend_error_in_article 2 'Article 108;\|' '108;' | \
-    amend_error_in_article 2 'Article 37;\|' '37;' | \
-    amend_error_in_article 2 ' \[•\]' ':' | \
-    sed -E ':start;s/^(\(2\).*)\[371\]/\1371)/;t start' | \
-    amend_error_in_article 2 '\[265\]' '371)' | \
-    amend_error_in_article 2 '\[1990\]' '1990)' | \
-    #Article 5
-    amend_error_in_article 5 'Article \[6\] Procedure of the Council\| ' '\n\nProcedure of the Council\n(6) ' | \
+    amend_error_in_article 2 'become adapted;' 'become adapted.' | \
+    amend_error_in_article 2 'Article 24' '24' | \
+    #Article 3
+    amend_error_in_article 3 'had the duty' 'has the duty' | \
+    amend_error_in_article 3 'public officer \[c\]' 'public officer; [c]' | \
+    amend_error_in_article 3 'and \[f\]' 'and; [f]' | \
+    amend_error_in_article 3 'sustainable development;' 'sustainable development:' | \
+    #Article 4
+    amend_error_in_article 4 '- ' ': ' | \
     #Article 6
     amend_error_in_article 6 ' The Authority' '' | \
-    #Article 21
-    amend_error_in_article 21 'thirteeth' 'thirteenth' | \
+    #Article 9
+    amend_error_in_article 9 '\[2\] \[p\]' '[2][p]' | \
+    #Article 10
+    amend_error_in_article 10 'authourity' 'authority' | \
+    amend_error_in_article 10 ' \[•\]' ':' | \
+    amend_error_in_article 10 'section 1 ' 'section 1' | \
+    amend_error_in_article 10 '. \[c\]' '; [c]' | \
+    amend_error_in_article 10 ': \[d\]' '; [d]' | \
+    amend_error_in_article 10 '\(c \)' '[c]' | \
     #Article 23
-    amend_error_in_article 23 '\(Cap.' '(Cap. 412).' | \
-    amend_error_in_article 23 '\[412\]' '412)' | \
-    sed -E ':start;s/^(\(23\).*)\[Corporations\]/\1(Corporations)/;t start' | \
+    sed -E 's/\[Corporations\]/(Corporations)/g' | \
+    amend_error_in_article 23 '\[5\]' '[3]' | \
+    #Article 24
+    sed -z 's/\n\n(12) 4]/ [4]/' | \
     #Article 28
-    amend_error_in_article 28 ' Provincial and District Environment Committees' '' | \
+    amend_error_in_article 28 'Provincial and District.*$' '' | \
     #Article 29
-    amend_error_in_article 29 'Article \[30\] Functions of Provincial and District Environment Committees\| ' '\n\nFunctions of Provincial and District Environment Committees\n(30) ' | \
+    amend_error_in_article 29 'NonGovernmental' 'Non-Governmental' | \
+    amend_error_in_article 29 '\[1\] Every' '[3] Every' | \
+    #Article 31
+    amend_error_in_article 31 '\[1\] \[b\]' '[1][b]' | \
+    amend_error_in_article 31 '\[6\]' '[5]' | \
     #Article 32
-    amend_error_in_article 33 'Article \[32\]\|' '32.' | \
-    #Article 38
-    amend_error_in_article 38 '\[Act No. 6 of 2006, s.' '\n\n[Act No. 6 of 2006, s. 77]' | \
+    amend_error_in_article 32 '9 \[3\]' '9[3]' | \
+    #Article 33
+    amend_error_in_article 33 'section 32 \[' 'section 32. \[' | \
+    #Article 35
+    amend_error_in_article 35 '\[1\]If' '[1] If' | \
+    #Article 37
+    sed -z 's/\n\n(15) 2]/ [2]/' | \
+    #Article 41
+    amend_error_in_article 41 '38 \[a\]' '38[a]' | \
     #Article 42
-    sed -E 's/ENVIRONMENT \[1\] No/ENVIRONMENT\n\n(42) [1] No/' | \
-    amend_error_in_article 42 '\[Act No. 5 of 2007, \[s\] 77, Act No. 6 of 2009, Sch.' '\n\n[Act No. 5 of 2007, s. 77, Act No. 6 of 2009, Sch.]' | \
-    #Article 47
-    sed -z 's/mountainous\n(/mountainous areas\n(/g' | \
-    sed -E 's/\) areas/)/g' | \
-    #Article 69
-    amend_error_in_article 69 ' \[•\]' ':' | \
-    #Article 70
-    amend_error_in_article 70 'Article \[71\] Functions of Standards and Enforcement Review Committee\| ' '\n\nFunctions of Standards and Enforcement Review Committee\n(71) ' | \
+    sed -z 's/\n\n(16) 3]/ [3]/' | \
+    amend_error_in_article 42 'resources;' 'resources.' | \
+    #Article 46
+    amend_error_in_article 46 'section 45 \[1\]' 'section 45[1]' | \
+    #Article 52
+    amend_error_in_article 52 ' Article' '' | \
+    amend_error_in_article 52 '. \[c\]' '; [c]' | \
+    #Article 58
+    amend_error_in_article 58 'fee..' 'fee.' | \
     #Article 81
     amend_error_in_article 81 '\[VI\]' 'VI.' | \
-    #Article 92
-    amend_error_in_article 92 "Article \[93\] Prohibition of discharge of hazardous substances, chemicals and\| materials or oil into the environment and spiller's liability " "\n\nProhibition of discharge of hazardous substances, chemicals and materials or oil into the environment and spiller's liability\n(93) " | \
-    amend_error_in_article 92 'the advise' 'the advice' | \
+    #Article 87
+    amend_error_in_article 87 '\[4\]' '[3]' | \
+    amend_error_in_article 87 '\[5\]' '[4]' | \
+    amend_error_in_article 87 '\[6\]' '[5]' | \
+    #Article 85
+    amend_error_in_article 84 'do; Article \[85\]' 'do.\n\n(85)' | \
     #Article 95
-    amend_error_in_article 95 's.' 's. 117.' | \
-    #Article 96
-    amend_error_in_article 96 's.' 's. 118.' | \
-    #Article 102
-    amend_error_in_article 102 '\[394\]' '394)' | \
-    #Article 104
-    amend_error_in_article 104 's.' 's. 78.' | \
-    #Article 105
-    amend_error_in_article 105 's.' 's. 79.' | \
-    #Article 106
-    sed -E 's/\(106\) \[Issue//' | \
-    sed -z 's/Repealed by Act No. 5 of 2007, \[s\] \[80\]\n/(106) Repealed by Act No. 5 of 2007, s. 80./' | \
-    #Article 107
-    amend_error_in_article 107 '\[Act' '\n\n[Act' | \
+    amend_error_in_article 94 'substances; Article \[95\]' 'substances. \n\n(95) ' | \
+    #Article 99
+    amend_error_in_article 99 '\[2\]' '[2] ' | \
+    amend_error_in_article 99 '\[8\]' '[3]' | \
+    amend_error_in_article 99 '\[9\]' '[4]' | \
+    #Article 108
+    amend_error_in_article 108 ' Article' '' | \
     #Article 112
-    amend_error_in_article 112 'Article \[116\]\|' '116.' | \
     amend_error_in_article 112 'acquifer' 'aquifer' | \
+    sed -z 's/\n\n(30) 5]/ [5]/' | \
+    amend_error_in_article 112 'section 116' 'section 116.' | \
+    #Article 117
+    amend_error_in_article 117 'DirectorGeneral' 'Director-General' | \
     #Article 118
     amend_error_in_article 118 'AttorneyGeneral' 'Attorney-General' | \
-    #Article 120
-    amend_error_in_article 120 'Article \[121\] Records to be kept\| ' '\n\nRecords to be kept\n(121) ' | \
-    #Article 124
-    amend_error_in_article 124 'AttorneyGeneral' 'Attorney-General' | \
-    #Article 126
-    amend_error_in_article 126 '\[80\]' '80)' | \
-    #Article 138
-    amend_error_in_article 138 'Article 58 of this Act;\|' '58 of this Act;' | \
-    #Article 144
-    amend_error_in_article 144 'Article \[145\] Offences by bodies corporate, Partnerships, Principals and Employers\| ' '\n\nOffences by bodies corporate, Partnerships, Principals and Employers\n(145) ' | \
+    #Article 127
+    amend_error_in_article 127 '; \[2\]' '. [2]' | \
+    amend_error_in_article 127 '\[1\] \[a\]' '[1][a]' | \
+    sed -z 's/\n\n(33) Tribunal;/; [ii] willfully interrupts the proceedings or commits any contempt of the Tribunal;/' | \
+    #Article 132
+    amend_error_in_article 132 '\[1\]When' '[1] When' | \
+    #Article 133
+    amend_error_in_article 133 '\[1\] It' '[2] It' | \
+    #Article 141
+    amend_error_in_article 141 'Kenya' 'Kenya; [g] withholds information or provides false information about the management of hazardous wastes, chemicals or radioactive substances;' | \
+    amend_error_in_article 141 'substances;' 'substances; commits an offence and shall, on conviction, be liable to a fine of not less than one million shillings, or to imprisonment for a term of not less than two years, or to both.' | \
+    sed -z 's/both.\n\n(35)/both./' | \
+    #Article 142
+    amend_error_in_article 142 'subsection \[1\]' 'subsections [1].' | \
+    amend_error_in_article 142 '\[1\] \[2\]' '[1] and [2]' | \
+    #Article 143
+    amend_error_in_article 143 'Fails' 'fails' | \
     #Article 148
-    amend_error_in_article 148 'FIRST SCHEDULE.*$' ''
+    amend_error_in_article 148 'FIRST SCHEDULE.*$' '' 
 }
 
 function amend_errors_in_headers {
-  sed -E 's/([^^])PART/\1\n\nPART/g' | \
-    sed -E 's/ADMINISTRATION The National Environment Council/ADMINISTRATION/' 
+  sed -E 's/(PART [A-Z]+): /\n\n\1 - /g' 
 }
 
 function preprocess_state_and_language_input_file {
@@ -125,10 +139,8 @@ function preprocess_state_and_language_input_file {
   cat "$input_file_path" | \
     remove_all_text_before_first_header | \
     prefix_article_numbers_with_article_literal | \
-    append_pipe_to_article_title | \
     replace_parentheses_around_article_delimiters_with_square_brackets | \
     apply_common_transformations_to_stdin "$language" | \
-    move_article_titles_above_article_bodies | \
     remove_all_text_after_last_article | \
     amend_errors_in_articles | \
     amend_errors_in_headers
